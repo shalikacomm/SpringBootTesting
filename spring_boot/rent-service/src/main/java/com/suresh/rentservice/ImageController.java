3ab06ae5fdc3ae92b1d761848062a9673421c78d
DESKTOP-Q6D1J4T\Lap.lk <amanda@gehilaw.com>
2022-09-06 21:44:09 +0530

    value 3 added

--- spring_boot/rent-service/src/main/java/com/suresh/rentservice/ImageController.java
+++ spring_boot/rent-service/src/main/java/com/suresh/rentservice/ImageController.java
@@ -3,7 +3,7 @@ package com.suresh.rentservice;
 public class ImageController {
 private void sout() {
 	// TODO Auto-generated method stub kamal addara arachchi test3
-Integer testValue = 1;
+Integer testValue = 3;
 
 }
 }
