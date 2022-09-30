38750993ae9b3c348434b45f0b3f7939888bbb62
DESKTOP-Q6D1J4T\Lap.lk <amanda@gehilaw.com>
2022-09-30 15:13:01 +0530

    sa

---
+++ spring_boot/rent-service/src/main/java/com/suresh/rentservice/gitHubController.java
@@ -0,0 +1,14 @@
+package com.suresh.rentservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitHubController {
+	
+	@RequestMapping("/rentmaster/greeting")
+	public String sayHello(){
+		return "<h1> this is a newly created file";
+	}
+
+}
