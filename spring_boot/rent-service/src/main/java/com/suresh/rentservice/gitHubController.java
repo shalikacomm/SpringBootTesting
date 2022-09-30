1c51a1d7b3f9bdb9e4a75596dc9e9b93c9129a34
DESKTOP-Q6D1J4T\Lap.lk <amanda@gehilaw.com>
2022-09-30 15:51:05 +0530

    nov branch changes

--- spring_boot/rent-service/src/main/java/com/suresh/rentservice/gitHubController.java
+++ spring_boot/rent-service/src/main/java/com/suresh/rentservice/gitHubController.java
@@ -8,7 +8,7 @@ public class GitHubController {
 	
 	@RequestMapping("/rentmaster/greeting")
 	public String sayHello(){
-		return "<h1> this is a newly created file";
+		return "<h1> this is a newly created file to vovember";
 	}
 
 }
