f598b754efb5f1c18f2ef6dc9cbbccebb588af84
DESKTOP-Q6D1J4T\Lap.lk <amanda@gehilaw.com>
2022-09-30 13:47:55 +0530

    chanegfs

--- spring_boot/rent-service/src/main/java/com/suresh/rentservice/greetingController.java
+++ spring_boot/rent-service/src/main/java/com/suresh/rentservice/greetingController.java
@@ -8,7 +8,7 @@ public class greetingController {
 	
 	@RequestMapping("/rentmaster/greeting")
 	public String sayHello(){
-		return "<h1> wow springboot you are awsome cloud change !!! </h1>";
+		return "<h1> this is local octobe rchanges</h1>";
 	}
 
 }
